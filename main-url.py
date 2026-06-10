c71a810db3beb333e1868e7196794f17ba913b53
rhh7272 <rhh7272@gmail.com>
2026-06-11 08:36:19 +0900

    Add files via upload

--- main-url.py
+++ main-url.py
@@ -3,8 +3,8 @@
 import os  # 내부에 들어가 있는 코어 라이브러리 (설치 없이 사용 가능)
 import tempfile
 import streamlit as st
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 from langchain_community.document_loaders import (
     YoutubeLoader,
